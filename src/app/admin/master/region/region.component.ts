9962e2cb37dc8d997eed8758dd2c1edc3c383751
Kittu-Mobiles <krishna@cortracker360.com>
2026-08-18 17:27:26 +0530

    Clockinout NEW WEB Kittu

--- src/app/admin/master/region/region.component.ts
+++ src/app/admin/master/region/region.component.ts
@@ -30,6 +30,7 @@ export class RegionComponent {
   pageSize = 5;
   totalPages = 0;
   pagedRegions: Region[] = [];
+  timeZones: { label: string; value: string }[] = [];
 
   constructor(
     private adminService: AdminService,
@@ -37,6 +38,7 @@ export class RegionComponent {
   ) {}
 
   ngOnInit(): void {
+    this.loadTimeZones();
     this.loadCompanies();
     this.loadRegions();
   }
@@ -85,6 +87,177 @@ export class RegionComponent {
       }
     });
   }
+  loadTimeZones(): void {
+  this.timeZones = [
+    { value: 'Etc/GMT+12', label: '(UTC-12:00) International Date Line West' },
+    { value: 'Etc/GMT+11', label: '(UTC-11:00) Coordinated Universal Time-11' },
+    { value: 'Pacific/Honolulu', label: '(UTC-10:00) Hawaii' },
+    { value: 'America/Adak', label: '(UTC-10:00) Aleutian Islands' },
+    { value: 'Pacific/Marquesas', label: '(UTC-09:30) Marquesas Islands' },
+    { value: 'America/Anchorage', label: '(UTC-09:00) Alaska' },
+    { value: 'Etc/GMT+9', label: '(UTC-09:00) Coordinated Universal Time-09' },
+
+    { value: 'America/Los_Angeles', label: '(UTC-08:00) Pacific Time (US & Canada)' },
+    { value: 'America/Tijuana', label: '(UTC-08:00) Baja California' },
+    { value: 'Etc/GMT+8', label: '(UTC-08:00) Coordinated Universal Time-08' },
+
+    { value: 'America/Phoenix', label: '(UTC-07:00) Arizona' },
+    { value: 'America/Chihuahua', label: '(UTC-07:00) Chihuahua, La Paz, Mazatlan' },
+    { value: 'America/Denver', label: '(UTC-07:00) Mountain Time (US & Canada)' },
+    { value: 'America/Whitehorse', label: '(UTC-07:00) Yukon' },
+
+    { value: 'America/Guatemala', label: '(UTC-06:00) Central America' },
+    { value: 'America/Chicago', label: '(UTC-06:00) Central Time (US & Canada)' },
+    { value: 'Pacific/Easter', label: '(UTC-06:00) Easter Island' },
+    { value: 'America/Mexico_City', label: '(UTC-06:00) Guadalajara, Mexico City, Monterrey' },
+    { value: 'America/Regina', label: '(UTC-06:00) Saskatchewan' },
+
+    { value: 'America/Bogota', label: '(UTC-05:00) Bogota, Lima, Quito, Rio Branco' },
+    { value: 'America/Cancun', label: '(UTC-05:00) Chetumal' },
+    { value: 'America/New_York', label: '(UTC-05:00) Eastern Time (US & Canada)' },
+    { value: 'America/Havana', label: '(UTC-05:00) Havana' },
+    { value: 'America/Indiana/Indianapolis', label: '(UTC-05:00) Indiana (East)' },
+    { value: 'America/Grand_Turk', label: '(UTC-05:00) Turks and Caicos' },
+
+    { value: 'America/Halifax', label: '(UTC-04:00) Atlantic Time (Canada)' },
+    { value: 'America/Caracas', label: '(UTC-04:00) Caracas' },
+    { value: 'America/Cuiaba', label: '(UTC-04:00) Cuiaba' },
+    { value: 'America/La_Paz', label: '(UTC-04:00) Georgetown, La Paz, Manaus, San Juan' },
+    { value: 'America/Santiago', label: '(UTC-04:00) Santiago' },
+
+    { value: 'America/St_Johns', label: '(UTC-03:30) Newfoundland' },
+
+    { value: 'America/Argentina/Buenos_Aires', label: '(UTC-03:00) City of Buenos Aires' },
+    { value: 'America/Asuncion', label: '(UTC-03:00) Asuncion' },
+    { value: 'America/Sao_Paulo', label: '(UTC-03:00) Brasilia' },
+    { value: 'America/Fortaleza', label: '(UTC-03:00) Cayenne, Fortaleza' },
+    { value: 'America/Montevideo', label: '(UTC-03:00) Montevideo' },
+    { value: 'America/Punta_Arenas', label: '(UTC-03:00) Punta Arenas' },
+    { value: 'America/Miquelon', label: '(UTC-03:00) Saint Pierre and Miquelon' },
+    { value: 'America/Araguaina', label: '(UTC-03:00) Araguaina' },
+
+    { value: 'Etc/GMT+2', label: '(UTC-02:00) Coordinated Universal Time-02' },
+    { value: 'America/Godthab', label: '(UTC-03:00) Greenland' },
+
+    { value: 'Atlantic/Azores', label: '(UTC-01:00) Azores' },
+    { value: 'Atlantic/Cape_Verde', label: '(UTC-01:00) Cabo Verde Is.' },
+
+    { value: 'UTC', label: '(UTC) Coordinated Universal Time' },
+    { value: 'Europe/London', label: '(UTC+00:00) Dublin, Edinburgh, Lisbon, London' },
+    { value: 'Atlantic/Reykjavik', label: '(UTC+00:00) Monrovia, Reykjavik' },
+    { value: 'Africa/Sao_Tome', label: '(UTC+00:00) Sao Tome' },
+
+    { value: 'Africa/Casablanca', label: '(UTC+01:00) Casablanca' },
+    { value: 'Europe/Rome', label: '(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna' },
+    { value: 'Europe/Prague', label: '(UTC+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague' },
+    { value: 'Europe/Warsaw', label: '(UTC+01:00) Sarajevo, Skopje, Warsaw, Zagreb' },
+    { value: 'Africa/Lagos', label: '(UTC+01:00) West Central Africa' },
+
+    { value: 'Europe/Athens', label: '(UTC+02:00) Athens, Bucharest' },
+    { value: 'Asia/Beirut', label: '(UTC+02:00) Beirut' },
+    { value: 'Africa/Cairo', label: '(UTC+02:00) Cairo' },
+    { value: 'Europe/Chisinau', label: '(UTC+02:00) Chisinau' },
+    { value: 'Asia/Gaza', label: '(UTC+02:00) Gaza, Hebron' },
+    { value: 'Africa/Johannesburg', label: '(UTC+02:00) Harare, Pretoria' },
+    { value: 'Europe/Helsinki', label: '(UTC+02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius' },
+    { value: 'Asia/Jerusalem', label: '(UTC+02:00) Jerusalem' },
+    { value: 'Africa/Juba', label: '(UTC+02:00) Juba' },
+    { value: 'Europe/Kaliningrad', label: '(UTC+02:00) Kaliningrad' },
+    { value: 'Africa/Khartoum', label: '(UTC+02:00) Khartoum' },
+    { value: 'Africa/Tripoli', label: '(UTC+02:00) Tripoli' },
+    { value: 'Africa/Windhoek', label: '(UTC+02:00) Windhoek' },
+    { value: 'Asia/Damascus', label: '(UTC+02:00) Damascus' },
+
+    { value: 'Asia/Amman', label: '(UTC+03:00) Amman' },
+    { value: 'Asia/Baghdad', label: '(UTC+03:00) Baghdad' },
+    { value: 'Europe/Istanbul', label: '(UTC+03:00) Istanbul' },
+    { value: 'Asia/Riyadh', label: '(UTC+03:00) Kuwait, Riyadh' },
+    { value: 'Europe/Minsk', label: '(UTC+03:00) Minsk' },
+    { value: 'Europe/Moscow', label: '(UTC+03:00) Moscow, St. Petersburg' },
+    { value: 'Africa/Nairobi', label: '(UTC+03:00) Nairobi' },
+    { value: 'Europe/Volgograd', label: '(UTC+03:00) Volgograd' },
+
+    { value: 'Asia/Tehran', label: '(UTC+03:30) Tehran' },
+
+    { value: 'Asia/Dubai', label: '(UTC+04:00) Abu Dhabi, Muscat' },
+    { value: 'Asia/Baku', label: '(UTC+04:00) Baku' },
+    { value: 'Europe/Samara', label: '(UTC+04:00) Izhevsk, Samara' },
+    { value: 'Indian/Mauritius', label: '(UTC+04:00) Port Louis' },
+    { value: 'Asia/Tbilisi', label: '(UTC+04:00) Tbilisi' },
+    { value: 'Asia/Yerevan', label: '(UTC+04:00) Yerevan' },
+
+    { value: 'Asia/Kabul', label: '(UTC+04:30) Kabul' },
+
+    { value: 'Asia/Ashgabat', label: '(UTC+05:00) Ashgabat, Tashkent' },
+    { value: 'Asia/Yekaterinburg', label: '(UTC+05:00) Ekaterinburg' },
+    { value: 'Asia/Karachi', label: '(UTC+05:00) Islamabad, Karachi' },
+    { value: 'Asia/Qyzylorda', label: '(UTC+05:00) Qyzylorda' },
+
+    // ⭐ INDIA
+    { value: 'Asia/Kolkata', label: '(UTC+05:30) Chennai, Kolkata, Mumbai, New Delhi' },
+    { value: 'Asia/Colombo', label: '(UTC+05:30) Sri Jayawardenepura' },
+
+    { value: 'Asia/Kathmandu', label: '(UTC+05:45) Kathmandu' },
+
+    { value: 'Asia/Almaty', label: '(UTC+06:00) Astana' },
+    { value: 'Asia/Dhaka', label: '(UTC+06:00) Dhaka' },
+    { value: 'Asia/Omsk', label: '(UTC+06:00) Omsk' },
+
+    { value: 'Asia/Yangon', label: '(UTC+06:30) Yangon (Rangoon)' },
+
+    { value: 'Asia/Bangkok', label: '(UTC+07:00) Bangkok, Hanoi, Jakarta' },
+    { value: 'Asia/Barnaul', label: '(UTC+07:00) Barnaul, Gorno-Altaysk' },
+    { value: 'Asia/Hovd', label: '(UTC+07:00) Hovd' },
+    { value: 'Asia/Krasnoyarsk', label: '(UTC+07:00) Krasnoyarsk' },
+    { value: 'Asia/Novosibirsk', label: '(UTC+07:00) Novosibirsk' },
+    { value: 'Asia/Tomsk', label: '(UTC+07:00) Tomsk' },
+
+    { value: 'Asia/Shanghai', label: '(UTC+08:00) Beijing, Chongqing, Hong Kong, Urumqi' },
+    { value: 'Asia/Irkutsk', label: '(UTC+08:00) Irkutsk' },
+    { value: 'Asia/Singapore', label: '(UTC+08:00) Kuala Lumpur, Singapore' },
+    { value: 'Australia/Perth', label: '(UTC+08:00) Perth' },
+    { value: 'Asia/Taipei', label: '(UTC+08:00) Taipei' },
+    { value: 'Asia/Ulaanbaatar', label: '(UTC+08:00) Ulaanbaatar' },
+
+    { value: 'Australia/Eucla', label: '(UTC+08:45) Eucla' },
+
+    { value: 'Asia/Chita', label: '(UTC+09:00) Chita' },
+    { value: 'Asia/Tokyo', label: '(UTC+09:00) Osaka, Sapporo, Tokyo' },
+    { value: 'Asia/Pyongyang', label: '(UTC+09:00) Pyongyang' },
+    { value: 'Asia/Seoul', label: '(UTC+09:00) Seoul' },
+    { value: 'Asia/Yakutsk', label: '(UTC+09:00) Yakutsk' },
+
+    { value: 'Australia/Adelaide', label: '(UTC+09:30) Adelaide' },
+    { value: 'Australia/Darwin', label: '(UTC+09:30) Darwin' },
+
+    { value: 'Australia/Brisbane', label: '(UTC+10:00) Brisbane' },
+    { value: 'Australia/Sydney', label: '(UTC+10:00) Canberra, Melbourne, Sydney' },
+    { value: 'Pacific/Guam', label: '(UTC+10:00) Guam, Port Moresby' },
+    { value: 'Australia/Hobart', label: '(UTC+10:00) Hobart' },
+    { value: 'Asia/Vladivostok', label: '(UTC+10:00) Vladivostok' },
+
+    { value: 'Australia/Lord_Howe', label: '(UTC+10:30) Lord Howe Island' },
+
+    { value: 'Pacific/Bougainville', label: '(UTC+11:00) Bougainville Island' },
+    { value: 'Asia/Magadan', label: '(UTC+11:00) Magadan' },
+    { value: 'Pacific/Norfolk', label: '(UTC+11:00) Norfolk Island' },
+    { value: 'Asia/Sakhalin', label: '(UTC+11:00) Sakhalin' },
+    { value: 'Pacific/Guadalcanal', label: '(UTC+11:00) Solomon Is., New Caledonia' },
+
+    { value: 'Asia/Anadyr', label: '(UTC+12:00) Anadyr, Petropavlovsk-Kamchatsky' },
+    { value: 'Pacific/Auckland', label: '(UTC+12:00) Auckland, Wellington' },
+    { value: 'Etc/GMT-12', label: '(UTC+12:00) Coordinated Universal Time+12' },
+    { value: 'Pacific/Fiji', label: '(UTC+12:00) Fiji' },
+
+    { value: 'Pacific/Chatham', label: '(UTC+12:45) Chatham Islands' },
+
+    { value: 'Etc/GMT-13', label: '(UTC+13:00) Coordinated Universal Time+13' },
+    { value: 'Pacific/Tongatapu', label: "(UTC+13:00) Nuku'alofa" },
+    { value: 'Pacific/Apia', label: '(UTC+13:00) Samoa' },
+
+    { value: 'Pacific/Kiritimati', label: '(UTC+14:00) Kiritimati Island' }
+  ];
+}
 
   // ✅ Save / Update Region
 onSubmit(): void {
@@ -202,48 +375,7 @@ if (!this.region.timeZoneId) {
   });
 
 }
-timeZones = [
-  {
-    label: 'India Standard Time (IST)',
-    value: 'Asia/Kolkata'
-  },
-  {
-    label: 'US Eastern Time (EST/EDT)',
-    value: 'America/New_York'
-  },
-  {
-    label: 'US Central Time (CST/CDT)',
-    value: 'America/Chicago'
-  },
-  {
-    label: 'US Mountain Time (MST/MDT)',
-    value: 'America/Denver'
-  },
-  {
-    label: 'US Pacific Time (PST/PDT)',
-    value: 'America/Los_Angeles'
-  },
-  {
-    label: 'Canada Eastern',
-    value: 'America/Toronto'
-  },
-  {
-    label: 'UK Time',
-    value: 'Europe/London'
-  },
-  {
-    label: 'UAE Time',
-    value: 'Asia/Dubai'
-  },
-  {
-    label: 'Singapore Time',
-    value: 'Asia/Singapore'
-  },
-  {
-    label: 'Australia Sydney',
-    value: 'Australia/Sydney'
-  }
-];
+
 getTimeZoneLabel(value: string): string {
   const timezone = this.timeZones.find(
     x => x.value === value
